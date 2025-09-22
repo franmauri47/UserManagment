5e30fb6cd00a6b24a8079fcd815d0e008d25f40f
Francisco Mauri <franciscomauri47@gmail.com>
2025-09-22 14:37:47 -0300

    Create GetUser query

---
+++ Application/Users/Queries/GetUserQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Dtos;
+using Domain.Entities;
+using Infrastructure.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Users.Queries;
+
+public record GetUserQuery(string? Name, string? Province, string? City) : IRequest<ResponseDto>;
+
+public class GetUserQueryHandler(
+    IUsersService usersService,
+    ILogger<GetUserQueryHandler> logger) : IRequestHandler<GetUserQuery, ResponseDto>
+{
+    public async Task<ResponseDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var query = await usersService.GetUsersByDataAsync(request.Name, request.Province, request.City, cancellationToken);
+            return new ResponseDto
+            {
+                ErrorCode = 0,
+                ErrorDescription = string.Empty,
+                Data = query
+            };
+        }
+        catch (Exception ex)
+        {
+            string errorMessage = $"An error occurred while searching users: {ex.Message}";
+            logger.LogError(errorMessage);
+            return new ResponseDto<List<User>>
+            {
+                ErrorCode = 1,
+                ErrorDescription = ex.Message
+            };
+        }
+    }
+}
---
+++ Application/Users/Validators/GetUserQueryValidator.cs
@@ -0,0 +1,23 @@
+﻿using Application.Users.Queries;
+using Domain.Helpers;
+using FluentValidation;
+
+namespace Application.Users.Validators;
+
+public class GetUserQueryValidator : AbstractValidator<GetUserQuery>
+{
+    public GetUserQueryValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(250).WithMessage(ValidationHelpers.MessageMaxLengthFieldByName)
+            .When(e => !string.IsNullOrEmpty(e.Name));
+
+        RuleFor(x => x.City)
+            .MaximumLength(100).WithMessage(ValidationHelpers.MessageMaxLengthFieldByName)
+            .When(e => !string.IsNullOrEmpty(e.City));
+
+        RuleFor(x => x.Province)
+            .MaximumLength(100).WithMessage(ValidationHelpers.MessageMaxLengthFieldByName)
+            .When(e => !string.IsNullOrEmpty(e.Province));
+    }
+}
